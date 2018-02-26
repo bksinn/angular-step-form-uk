cd05bb1b2175c797e4c8ba0d5438520f5989a335
Bharat Singh <bksingh@g.cofc.edu>
2018-02-26 17:49:19 -0500

    Scaffolding initial app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
+/* App Root */
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+/* Feature Components */
+import { PersonalComponent } from './personal/personal.component';
+import { WorkComponent } from './work/work.component';
+import { AddressComponent } from './address/address.component';
+import { ResultComponent } from './result/result.component';
+
+/* Routing Module */
+import { AppRoutingModule } from './app-routing.module';
+
+/* Shared Service */
+import { FormDataService } from './data/formData.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  imports: [BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  providers: [{ provide: FormDataService, useClass: FormDataService }],
+  declarations: [AppComponent, NavbarComponent, PersonalComponent, WorkComponent, AddressComponent, ResultComponent],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
